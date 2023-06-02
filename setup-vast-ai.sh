3d184abe67ede3589011a7989c131b77a43dc98b
anton <mail@antonkuzmin.ru>
2023-06-02 18:55:15 +0700

    fix lora-models path

--- setup-vast-ai.sh
+++ setup-vast-ai.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "[setup-vast-ai.sh] Started"
 
-ROOT=/stable-diffusion-webui
+SDROOT=/stable-diffusion-webui
 #CONTROLNET_REPO=https://github.com/Mikubill/sd-webui-controlnet
 REMOVEBG_REPO=https://github.com/Pakuronn/sd-webui-bgremove
 # new controlnet:
@@ -17,7 +17,7 @@ rm start.sh
 wget -O start.sh https://raw.githubusercontent.com/Pakuronn/maigic_bot_public/master/start.sh
 chmod +x start.sh
 
-cd $ROOT || exit 1
+cd $SDROOT || exit 1
 
 # Patch arguments
 sed -i 's/webui.sh -f"/webui.sh -f --api --api-auth pensipens:F743kFD234! --port 3000 --xformers --listen --enable-insecure-extension-access"/' ./relauncher.py
@@ -35,7 +35,7 @@ mkdir -p /hn-models
 echo "[setup-vast-ai.sh] Installing BgRemove extension..."
 cd extensions || exit 2
 [[ -d "sd-webui-bgremove" ]] || git clone "$REMOVEBG_REPO"
-cd $ROOT || exit 1
+cd $SDROOT || exit 1
 
 # DOWNLOAD MODELS:
 
@@ -45,25 +45,25 @@ echo "[setup-vast-ai.sh] Downloading models..."
 #cd models/Stable-diffusion || exit 3
 cd /sd-models || exit 3
 wget -nc -O deliberate_v2.safetensors https://civitai.com/api/download/models/15236
-cd $ROOT || exit 1
+cd $SDROOT || exit 1
 
 # controlnet model
 # cd extensions/sd-webui-controlnet/models || exit 2
 cd /cn-models || exit 2
 wget -nc "$CONTROLNET_MODEL"
-cd $ROOT || exit 1
+cd $SDROOT || exit 1
 
 # Lora для Noir
-cd lora-models || exit 4
+cd /lora-models || exit 4
 wget -nc -O mo.safetensors https://civitai.com/api/download/models/67892
-cd $ROOT || exit 1
+cd $SDROOT || exit 1
 
 # Lora для Anime
-cd lora-models || exit 5
+cd /lora-models || exit 5
 wget -nc -O satoshiUrushihara_urushisatoV15.safetensors https://civitai.com/api/download/models/24272
-cd $ROOT || exit 1
+cd $SDROOT || exit 1
 
 # Hypernetwork для Anime
-cd hn-models || exit 6
+cd /hn-models || exit 6
 wget -nc -O incaseStyle_incaseAnythingV3.pt https://civitai.com/api/download/models/5938
-cd $ROOT || exit 1
+cd $SDROOT || exit 1
